6cdb379163edfceda56787a4dbd282350a8847db
abhaysharma20 <51924043+abhaysharma20@users.noreply.github.com>
2026-06-28 10:54:41 +0000

    Deploying to main from @ abhaysharma20/abhaysharma20.github.io@bf977b915be074bff6a39dcfaf5423e11d1c6c64 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "998564200"
+    serviceWorkerVersion: "2284462294"
   }
 });
